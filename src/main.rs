90eb3de674918700a8454bdd43fbe141e4bef6ac
Allen <allen.davisswing@gmail.com>
2024-07-05 21:45:10 -0400

    first commit

---
+++ src/main.rs
@@ -0,0 +1,45 @@
+#[allow(unused_imports)]
+use std::io::{self, Write};
+
+use shell::{Command, Shell};
+
+mod shell;
+
+fn main() {
+    // Define the commands
+    let mut shell = Shell::new();
+
+    // Empty command
+    shell.add_command(Command::new("", |_, _| {}));
+
+    shell.add_command(Command::new("echo", |args, _| {
+        let rest = args.iter().skip(1).fold(String::new(), |acc, arg| acc + " " + arg);
+        println!("{}", rest.trim());
+    }));
+
+    shell.add_command(Command::new("exit", |args, _| {
+        let status = args.get(1).unwrap_or(&String::from("0")).parse::<i32>().unwrap_or(0);
+        std::process::exit(status);
+    }));
+
+    shell.add_command(Command::new("type", |args, shell| {
+        let command = args.get(1);
+        if command.is_none() {
+            println!("type: missing argument");
+            return;
+        }
+        let command = command.unwrap();
+        if shell.commands.contains_key(command.as_str()) {
+            println!("{} is a shell builtin", command);
+            return;
+        }
+        if shell.get_system_handler(command).is_some() {
+            println!("{} is a system command", command);
+            return;
+        }
+        println!("{}: not found", command);
+    }));
+
+    // Run the shell
+    shell.run();
+}
