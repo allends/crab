f46a9460f33bf9a457c856ff2e5e3f2e35200394
Allen <allen.davisswing@gmail.com>
2024-07-06 20:52:56 -0400

    add in more commands

--- src/main.rs
+++ src/main.rs
@@ -36,11 +36,48 @@ fn main() {
             println!("{} is a shell builtin", command);
             return;
         }
-        if shell.get_system_handler(command).is_some() {
-            println!("{} is a system command", command);
+
+        let system_handler = shell.get_system_handler(command);
+
+        if system_handler.is_some() {
+            let system_handler = system_handler.unwrap();
+            println!("{} is {}/{}", command, system_handler, command);
             return;
         }
         println!("{}: not found", command);
+    }));
+
+    shell.add_command(Command::new("pwd", |_, _| {
+        let path = std::env::current_dir().unwrap();
+        println!("{}", path.display());
+    }));
+
+    shell.add_command(Command::new("cd", |args, _| {
+        let path = args.get(1);
+        if path.is_none() {
+            println!("cd: missing argument");
+            return;
+        }
+        let path = path.unwrap();
+
+        if path == "~" {
+            let home = std::env::var("HOME");
+            if home.is_err() {
+                println!("cd: HOME not set");
+                return;
+            }
+            let home = home.unwrap();
+            let result = std::env::set_current_dir(&home);
+            if result.is_err() {
+                println!("cd: {}: No such file or directory", home);
+            }
+            return;
+        }
+
+        let result = std::env::set_current_dir(path);
+        if result.is_err() {
+            println!("cd: {}: No such file or directory", path);
+        }
     }));
 
     // Run the shell
